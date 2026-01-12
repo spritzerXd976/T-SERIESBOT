b130aaa0f5dd633901fceaa7dff5a65df72f1f28
ɪᴍ-ɴᴏᴛʏᴄᴏᴅᴇʀ <130166544+im-notycoder@users.noreply.github.com>
2026-01-12 21:19:28 +0530

    Import VideosSearch from py_yt

--- PURVIMUSIC/utils/thumbnails.py
+++ PURVIMUSIC/utils/thumbnails.py
@@ -14,6 +14,7 @@ from youtubesearchpython.__future__ import VideosSearch
 from PURVIMUSIC import app
 from config import YOUTUBE_IMG_URL
 
+from py_yt import VideosSearch
 
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
