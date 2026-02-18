e5e434b66d372458ab6300a8930d1057bad762b4
spritzerXd976 <168266234+spritzerXd976@users.noreply.github.com>
2026-02-18 16:00:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/im-notycoder/EXOTICX",
+    "https://github.com/",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
